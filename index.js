873d7ea0dd4ae6c685b6b6e4de08f76e3dbe4ee4
Aizen <iansky3311@gmail.com>
2023-03-23 11:45:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(act ="roller-skate"){
+    console.log(`This Saturday, I want to ${act}!`);
+    return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(mAct="go to the office"){
+    console.log(`This Monday, I will ${mAct}.`);
+    return `This Monday, I will ${mAct}.`;
+}
+function wrapAdjective(sym ='*'){
+    return function(adjective = "special") {
+    return `You are ${sym}${adjective}${sym}!`;
+    }
+}
